0909fcd2ec810c59719a8b3e8715d4fec5ec09ae
nishka2310 <nishita.kambli@vit.edu.in>
2026-06-17 21:58:50 +0530

    Biomedical Equipment Management System Complete

--- equipment/urls.py
+++ equipment/urls.py
@@ -2,6 +2,41 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.equipment_list, name='equipment_list'),
-    path('add/', views.add_equipment, name='add_equipment'),
+
+    path(
+        '',
+        views.equipment_list,
+        name='equipment_list'
+    ),
+
+    path(
+        'add/',
+        views.add_equipment,
+        name='add'
+    ),
+
+    path(
+        'edit/<int:id>/',
+        views.edit_equipment,
+        name='edit'
+    ),
+
+    path(
+        'delete/<int:id>/',
+        views.delete_equipment,
+        name='delete'
+    ),
+    path(
+        'dashboard/',
+        views.dashboard,
+        name='dashboard'
+    ),
+
+
 ]
+
+path(
+'dashboard/',
+views.dashboard,
+name='dashboard'
+)
--- equipment/views.py
+++ equipment/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render, redirect
 from .models import Equipment
 from .forms import EquipmentForm
+from django.shortcuts import get_object_or_404
 
 def equipment_list(request):
-    equipments = Equipment.objects.all()
-    return render(request, 'equipment/equipment_list.html', {'equipments': equipments})
 
+    query = request.GET.get('q')
+
+    if query:
+        equipments = Equipment.objects.filter(
+            name__icontains=query
+        )
+
+    else:
+        equipments = Equipment.objects.all()
+
+    return render(
+        request,
+        'equipment/equipment_list.html',
+        {
+            'equipments': equipments
+        }
+    )
 
 def add_equipment(request):
     if request.method == 'POST':
@@ -17,3 +33,53 @@ def add_equipment(request):
         form = EquipmentForm()
 
     return render(request, 'equipment/add_equipment.html', {'form': form})
+# EDIT
+def edit_equipment(request, id):
+    equipment = get_object_or_404(Equipment, id=id)
+
+    if request.method == 'POST':
+        form = EquipmentForm(request.POST, instance=equipment)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/equipment/')
+
+    else:
+        form = EquipmentForm(instance=equipment)
+
+    return render(
+        request,
+        'equipment/edit_equipment.html',
+        {'form': form}
+    )
+
+
+# DELETE
+def delete_equipment(request, id):
+    equipment = get_object_or_404(Equipment, id=id)
+
+    equipment.delete()
+
+    return redirect('/equipment/')
+
+def dashboard(request):
+
+    total = Equipment.objects.count()
+
+    available = Equipment.objects.filter(
+        status='Available'
+    ).count()
+
+    maintenance = Equipment.objects.filter(
+        status='Maintenance'
+    ).count()
+
+    return render(
+        request,
+        'equipment/dashboard.html',
+        {
+            'total': total,
+            'available': available,
+            'maintenance': maintenance
+        }
+    )
